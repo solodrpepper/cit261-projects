3fb6ffd3da047aed6933c075385658e6561eaaa9
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-05-08 10:36:30 -0600

    Added a class to topic two

--- javascript/topic_02.js
+++ javascript/topic_02.js
@@ -3,3 +3,26 @@
     JavaScript – Object Creation Functions, Properties, Methods, Instantiation
 */
 
+// Let's start by creating an object
+class Person {
+  constructor(firstName, middleInitial, lastName, age) {
+    this._firstName     = firstName;
+    this._middleInitial = middleInitial;
+    this._lastName      = lastName;
+    this._age           = age;
+    this._wholeName     = createWholeName();
+  }
+
+  // this method will return the Users whole name
+  createWholeName() {
+    let mid = !this._middleInitial ? `` : this._middleInitial;
+    return (`${this._firstName} ${mid}. ${this._lastName}`);
+  }
+
+  // This will be a welcome message used for welcoming users
+  welcomeMessage() {
+    return (`Welcome, ${this._wholeName}!`);
+  }
+
+
+}
