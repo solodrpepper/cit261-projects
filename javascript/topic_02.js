5fe689f750371ee21fb1b248a7410accd7753492
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-05-10 14:32:24 -0600

    cleaned up css on topic 2

--- javascript/topic_02.js
+++ javascript/topic_02.js
@@ -5,24 +5,24 @@
 
 // Let's start by creating an object
 class Person {
-  constructor(firstName, middleInitial, lastName, age) {
-    this._firstName     = firstName;
-    this._middleInitial = middleInitial;
-    this._lastName      = lastName;
-    this._age           = age;
-  }
+   constructor(firstName, middleInitial, lastName, age) {
+      this._firstName = firstName;
+      this._middleInitial = middleInitial;
+      this._lastName = lastName;
+      this._age = age;
+   }
 }
 
 // this method will return the Users whole name
 Person.prototype.createWholeName = function() {
-  let mid = !this._middleInitial ? `` : this._middleInitial;
-  this._wholeName = (`${this._firstName} ${mid}. ${this._lastName}`);
-  return this._wholeName;
-}
+   let mid = !this._middleInitial ? `` : `${this._middleInitial}.`;
+   this._wholeName = `${this._firstName} ${mid} ${this._lastName}`;
+   return this._wholeName;
+};
 
 // This will be a welcome message used for welcoming users
 Person.prototype.welcomeMessage = function() {
-  const wholeName = this.createWholeName();
-  const welcome_Message = (`Welcome, ${wholeName}!`);
-  return welcome_Message;
-}
+   const wholeName = this.createWholeName();
+   const welcome_Message = `Welcome, ${wholeName}!`;
+   return welcome_Message;
+};
