04f1735a5aa394487b2238686d99c7e9a8481ccc
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-05-15 11:29:41 -0600

    Finished topic number 5

--- javascript/topic_01.js
+++ javascript/topic_01.js
@@ -4,14 +4,24 @@
 
 // Clear function
 function clearCell(sectionname) {
-   let cellContents = `<div></div>`;
-   document.getElementById(sectionname).innerHTML = cellContents;
+   let cellContents = ``;
+   let contentSection = document.getElementById(sectionname);
+   contentSection.innerHTML = cellContents;
+
+   let section = contentSection.parentElement;
+
+   for (let node of section.children) {
+      // console.log(
+      //    `This is the output for the clear selection: ${node.tagName.toLowerCase()}`
+      // );
+      if (node.tagName.toLowerCase() == `input`) {
+         node.value = ``;
+      }
+   }
 }
 
 // Good ole fashion "For Loop"
 function forLoopTest() {
-   // TODO: Create a loop that will pause each iteration
-
    let finalText = '';
 
    for (let i = 0; i < 64; i++) {
@@ -26,12 +36,12 @@ function whileLoopTest() {
    sInput = document.getElementById('start_num');
    eInput = document.getElementById('end_condition');
 
-   console.table([sInput, eInput]);
+   // console.table([sInput, eInput]);
    // get user start value input and set to zero if not set
-   let start = sInput == '' ? 0 : sInput.value;
+   let start = sInput == '' ? 0 : parseInt(sInput.value);
    // check for and set user defined end condition
    //    (if not set, default to 81)
-   let end = eInput == '' ? 81 : eInput.value;
+   let end = eInput == '' ? 81 : parseInt(eInput.value);
    // create variable to hold final output of the loop
    let finalText = '';
    // check to make suer that the user doesn't create an infinite loop
@@ -47,7 +57,7 @@ function whileLoopTest() {
    }
    // now make sure end isn't greater than start
    if (start >= end) {
-      console.log(`Am I getting in here? ${start >= end}`);
+      // console.log(`Am I getting in here? ${start >= end}`);
       document.getElementById(
          'while-loop-test'
       ).innerHTML = `Start must be smaller than end!`;
@@ -70,7 +80,7 @@ function doLoopTest() {
    doEInput = document.getElementById('do_end_condition');
    // check for and set user defined end condition
    //    (if not set, default to 0)
-   let end = doEInput == '' ? 0 : doEInput.value;
+   let end = doEInput == '' ? 0 : parseInt(doEInput.value);
    // create variable to hold final output of the loop
    let finalText = '';
    //    it's valid input. (no negative inputs)
@@ -104,4 +114,9 @@ function forInLoopTest() {
 
    // now send the final text to the element it'll be displayed in
    document.getElementById('for-in-test').innerHTML = finalText;
+}
+
+// for of loop test example
+function forOfLoopTest() {
+   // create an array to iterate through
 }
---
+++ javascript/topic_05.js
@@ -0,0 +1,130 @@
+/**********************************************************
+ * This file will pertain to Local Storage using JavaScript
+ */
+
+// this section is taken from developer.mozilla to check
+//   if we can even use local storage
+function storageAvailable(type) {
+   var storage;
+   try {
+      storage = window[type];
+      var x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+   } catch (e) {
+      return (
+         e instanceof DOMException &&
+         // everything except Firefox
+         (e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+         // acknowledge QuotaExceededError only if there's something already stored
+         (storage && storage.length !== 0)
+      );
+   }
+}
+
+// We will first grab all of the important elements
+// input elements
+const inputKey = document.getElementById('inputKey');
+const inputData = document.getElementById('inputData');
+// output elements
+const lsOutput = document.getElementById('lsOutput');
+const ssOutput = document.getElementById('ssOutput');
+// buttons
+const btnInsert = document.getElementById('btnInsert');
+const btnClearAllData = document.getElementById('btnClearAllData');
+
+/**********************************************************
+ * This function will add data to the local storage
+ */
+function saveToLocalStroage() {
+   // check to see if we can use local storage
+   if (storageAvailable('localStorage')) {
+      // Yippee! We can use localStorage awesomeness
+      // TODO: Save to local storage
+      const key = inputKey.value;
+      const data = inputData.value;
+
+      console.log(key);
+      console.log(data);
+
+      if (key && data) {
+         localStorage.setItem(key, data);
+      }
+   } else {
+      // Too bad, no localStorage for us
+   }
+}
+
+/**********************************************************
+ * This function will add data to the session storage
+ */
+function saveToSessionStorage() {
+   // check to see if we can use session storage
+   if (storageAvailable('sessionStorage')) {
+      // Yippee! We can use sessionStorage awesomeness
+      // TODO: Save to session storage
+      const key = inputKey.value;
+      const data = inputData.value;
+
+      console.log(key);
+      console.log(data);
+
+      if (key && data) {
+         sessionStorage.setItem(key, data);
+      }
+   } else {
+      // Too bad, no sessionStorage for us
+   }
+}
+
+/**********************************************************
+ * This function will clear data from the local storage
+ */
+function clearLocalStorage() {
+   // TODO: clear to local storage
+   localStorage.clear();
+}
+
+/**********************************************************
+ * This function will clear data from the session storage
+ */
+function clearSessionStorage() {
+   // TODO: clear session storage
+   sessionStorage.clear();
+}
+
+function saveStorage() {
+   saveToLocalStroage();
+   saveToSessionStorage();
+   location.reload();
+}
+
+function clearStorage() {
+   clearLocalStorage();
+   clearSessionStorage();
+   location.reload();
+}
+
+window.onload = function() {
+   for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const data = localStorage.getItem(key);
+
+      lsOutput.innerHTML += `${key}: ${data}<br />`;
+   }
+
+   for (let i = 0; i < sessionStorage.length - 1; i++) {
+      const key = sessionStorage.key(i);
+      const data = sessionStorage.getItem(key);
+
+      ssOutput.innerHTML += `${key}: ${data}<br />`;
+   }
+};
