e9b4864a4407f85e01c68680b725eb4b7a31c2bf
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-05-22 11:17:28 -0600

    Let's see if that works

--- javascript/topic_03.js
+++ javascript/topic_03.js
@@ -99,6 +99,26 @@ window.addEventListener('load', () => {
          long = position.coords.longitude;
          lat = position.coords.latitude;
       });
+      // now grab the weather in their area
+      //Create the XHR Object
+      let xhr = new XMLHttpRequest();
+      //Call the open function, GET-type of request, url, true-asynchronous
+      xhr.open(
+         'GET',
+         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&long=${long}&APPID=${appId}&units=${units}`,
+         true
+      );
+      //call the onload
+      xhr.onload = function() {
+         //check if the status is 200(means everything is okay)
+         if (this.status === 200) {
+            //return server response as an object with JSON.parse
+            let result = JSON.parse(this.responseText);
+            init(result);
+         }
+      };
+      //call send
+      xhr.send();
    } else {
       // do nothing right now
    }
